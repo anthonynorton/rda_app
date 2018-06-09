61e4c2a87252a8bffc30512d6f6a5badb92d9e71
ananantn <nortonanthonya@gmail.com>
2018-06-09 12:49:25 -0600

    More practice updates

--- src/App.js
+++ src/App.js
@@ -8,14 +8,78 @@ class App extends Component {
       { name: "Max", age: 28 },
       { name: "Manu", age: 29 },
       { name: "Stephanie", age: 26 }
-    ]
+    ],
+    otherState: "Some other value",
+    showPersons: false
   }
-  
-  switchNameHandler = () => {
-    
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "B", age: 29 },
+        { name: "C", age: 27 }
+      ]
+    })
+  }
+
+  nameChangeHandler = (event, id) => {
+    const person = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.person[personsIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  }
+
+  deletePersonHandler = () => {
+    // const persons = this.state.persons.slice();
+    const person = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons, persons });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   }
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1x solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        < div >
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              change={(event) => this.nameChangeHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -24,10 +88,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={}>Switch Name</button>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29">Hobbies: Racing</Person>
-        <Person name="Stepahnie" age="26"/>
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
   }
